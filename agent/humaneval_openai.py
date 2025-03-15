30a7ec4c0bc2d2c9837ba24b703bf992dc124f1b
Abderrahmane Brahim <114669652+abderrahmane-br@users.noreply.github.com>
2025-03-15 03:46:19 +0000

    initialized main files

---
+++ agent/humaneval_openai.py
@@ -0,0 +1,10 @@
+from benchflow import BaseAgent
+
+
+class HumanevalAgent(BaseAgent):
+
+    def __init__(self):
+        super().__init__()
+
+    def call_api(self, task_step_inputs):
+        pass
